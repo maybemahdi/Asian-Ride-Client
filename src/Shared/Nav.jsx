22d6752b89810dc7239eec3b8c5dad3067ff9018
Mahdi Hasan Mahi <mh7266391@gmail.com>
2024-04-25 20:13:05 +0600

    nav added

---
+++ src/Shared/Nav.jsx
@@ -0,0 +1,103 @@
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "border focus:bg-[#fff] focus:text-[#a93fda] text-[#a93fda] border-[#a93fda] no-underline bg-white"
+              : "text-black no-underline"
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "border focus:bg-[#fff] focus:text-[#a93fda] text-[#a93fda] border-[#a93fda] no-underline bg-white"
+              : "text-black no-underline"
+          }
+          to={"/all-tourist-spot"}
+        >
+          All Tourists Spot
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "border focus:bg-[#fff] focus:text-[#a93fda] text-[#a93fda] border-[#a93fda] no-underline bg-white"
+              : "text-black no-underline"
+          }
+          to={"/add-tourist-spot"}
+        >
+          Add Tourists Spot
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "pending"
+              : isActive
+              ? "border focus:bg-[#fff] focus:text-[#a93fda] text-[#a93fda] border-[#a93fda] no-underline bg-white"
+              : "text-black no-underline"
+          }
+          to={"/my-list"}
+        >
+          My List
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
